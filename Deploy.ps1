c225ff30ff675741787e8f4ade75198000af9cd8
smallinov <ryan.smallegan@gmail.com>
2016-04-24 21:21:28 -0400

    shorten name

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,3 +1,3 @@
 $packageweb = $artifacts.values | Where-Object { $_.path -like '*Albums.Web.zip' }
 
-iex C:\Program Files (x86)\AWS Tools\Deployment Tool\awsdeploy.exe -DDeploymentPackage=$($packageweb.path) -DAWSAccessKey=AKIAJVV2NXH6KFJQR4OA -DAWSSecretKey=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 awseb-zpdev.txt
+iex C:\PROGRA~2\AWS Tools\Deployment Tool\awsdeploy.exe -DDeploymentPackage=$($packageweb.path) -DAWSAccessKey=AKIAJVV2NXH6KFJQR4OA -DAWSSecretKey=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 awseb-zpdev.txt
