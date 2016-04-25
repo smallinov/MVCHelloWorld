75d4541285d6e40bd55c586f006bb3e3e968c69a
smallinov <ryan.smallegan@gmail.com>
2016-04-24 21:28:11 -0400

    change

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,3 +1,3 @@
 $packageweb = $artifacts.values | Where-Object { $_.path -like '*Albums.Web.zip' }
 
-iex awsdeploy.exe -DDeploymentPackage=$($packageweb.path) -DAWSAccessKey=AKIAJVV2NXH6KFJQR4OA -DAWSSecretKey=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 awseb-zpdev.txt
+awsdeploy.exe -DDeploymentPackage=$($packageweb.path) -DAWSAccessKey=AKIAJVV2NXH6KFJQR4OA -DAWSSecretKey=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 awseb-zpdev.txt
